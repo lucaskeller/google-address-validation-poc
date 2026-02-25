9c9ec87b974ab7fc0712cf11ba53263db40c941a
c-lkeller <c-lkeller@thezebra.com>
2026-02-25 11:08:32 -0300

    adding pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/google-address-validation-poc/',
   plugins: [react()],
   test: {
     environment: 'jsdom'
